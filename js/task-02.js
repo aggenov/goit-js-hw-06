8a8167ee359ff29b58d5ce58e16a64418293b852
aggenov <a.g.genov@gmail.com>
2023-03-16 11:49:32 +0200

    1,2 tasks

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngr = document.querySelector("#ingredients");
+
+const nameIngredients = ingredients.map((ingredient) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  console.log(listItem.textContent);
+  return listItem;
+});
+console.log(nameIngredients);
+listIngr.append(...nameIngredients);
